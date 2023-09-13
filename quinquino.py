687e68fb46aa348f72f2034a9083066794ca8bf6
shanelang20 <144906694+shanelang20@users.noreply.github.com>
2023-09-13 04:45:33 -0700

    Add files via upload
    MY SELF INFORMATION

---
+++ quinquino.py
@@ -0,0 +1,12 @@
+# My Self Information
+print("Complete Name: Shane Christian N. Quinquino")
+
+print("Course: BS Computer science")
+
+print("Section: CS 1E")
+
+print("Age: 19 ")
+
+print("Birthday: September 20, 2003")
+
+print("Complete Home Address: Zone 8, Natumolan, Tagoloan, Misamis Oriental")
